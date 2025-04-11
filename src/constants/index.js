c85059f3547b2056351ac5a342fa4d947c655b05
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2025-04-11 18:56:57 +0500

    add new expernice

---
+++ src/constants/index.js
@@ -0,0 +1,286 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,  
+  suparco,
+  mern,
+  reactjslogo,
+  carrent,
+  jobit,
+  sportStream,
+  threejs,
+  frontEnd,
+  cardle,
+  thecustomcases,
+  japnessdreamtour
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Problem Solver",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  
+  {
+    title: "Backend Developer",
+    company_name: "Paradigm Digitals",
+    icon: nodejs,
+    iconBg: "#E6DEDD",
+    date: "Apr 2025 - Current",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "MERN Stack Developer",
+    company_name: "Code Dazzle",
+    icon: mern,
+    iconBg: "#383E56",
+    date: "Apr 2023 - Feb 2025",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "MERN Stack Developer",
+    company_name: "Innovatve Tech",
+    icon: mern,
+    iconBg: "#E6DEDD",
+    date: "May 2022 - Feb 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+
+  {
+    name: "The Custom Cases",
+    description:
+      "Capture your favorite memories with your own, one-of-one phone case. CaseCobra allows you to protect your memories, not just your phone case.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nextjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+    ],
+    image: thecustomcases,
+    source_code_link: "https://thecustomcases.store/",
+  },
+  
+  {
+    name: "CardLE Banking App",
+    description:
+      "Web-based platform that allows users to access and manage your account and transactions efficiently.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "nextjs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: cardle,
+    source_code_link: "https://cardle-bank.vercel.app/",
+  },
+  {
+    name: "Japanese Dream Tours",
+    description:
+      "The gateway to discovering the wonders of Japan and beyond. Whether you want to immerse yourself in the rich history of Kyoto, marvel at the modern energy of Tokyo, or experience the natural beauty of Hokkaido.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: japnessdreamtour,
+    source_code_link: "https://japnessdreamtour.vercel.app",
+  },
+  {
+    name: "Sport Streaming App",
+    description:
+      "A comprehensive sport streaming platform that allows users to watch match, and movie, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: sportStream,
+    source_code_link: "https://sport-stream.netlify.app",
+  },
+  
+  
+];
+
+export { services, technologies, experiences, testimonials, projects };
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0c0c0c",
+        secondary: "#AFABAB",
+        tertiary: "#272511",
+        "black-100": "#25210dbe",
+        "black-200": "#090325",
+        "white-100": "#f3f3f3",
+      },
+      boxShadow: {
+        card: "0px 35px 120px -15px #1F1C14",
+      },
+      screens: {
+        xs: "480px",
+       
+      },
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/herobg3.png')",
+      },
+    },
+  },
+  plugins: [],
+};
